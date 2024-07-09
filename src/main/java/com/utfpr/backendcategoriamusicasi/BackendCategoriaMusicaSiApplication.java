cb7d95049312604d1747f186e9397f238ef68607
Cyber-Barbarian <59051943+Cyber-Barbarian@users.noreply.github.com>
2024-07-09 08:45:08 -0300

    Add files via upload

---
+++ src/main/java/com/utfpr/backendcategoriamusicasi/BackendCategoriaMusicaSiApplication.java
@@ -0,0 +1,13 @@
+package com.utfpr.backendcategoriamusicasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCategoriaMusicaSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCategoriaMusicaSiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utfpr/backendcategoriamusicasi/BackendCategoriaMusicaSiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.backendcategoriamusicasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCategoriaMusicaSiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
